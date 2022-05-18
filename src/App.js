faee0b60637ae159928b0d71203c87a547c567df
gamitej <gamitejpratapsingh998@gmail.com>
2022-05-18 17:23:39 +0530

    Material-UI Theme Palette

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
+import "./App.css";
+import { Button,  Typography } from "@mui/material";
+import { Add, Settings } from "@mui/icons-material";
+
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Button variant="text">Text</Button>
+      <Button startIcon={<Settings />} variant="contained">
+        SETTINGS
+      </Button>
+      <Button startIcon={<Add />} variant="success">
+        ADD NEW POST
+      </Button>
+      <Button variant="outlined">Outlined</Button>
+      <Typography variant="h1">Heading</Typography>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "skyblue",
+          color: "#888",
+          margin: 5,
+          "&:hover": {
+            backgroundColor: "lightblue",
+          },
+        }}
+      >
+        Outlined
+      </Button>
     </div>
   );
 }
