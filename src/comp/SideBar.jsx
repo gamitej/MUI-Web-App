7b29a43451977d4e5124f315a345459206cc4fe4
gamitej <gamitejpratapsingh998@gmail.com>
2022-05-19 14:42:11 +0530

    SideBar.jsx Position Fixed

--- src/comp/SideBar.jsx
+++ src/comp/SideBar.jsx
@@ -16,24 +16,28 @@ const SideBar = () => {
   return (
     <>
       <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
-        <List>
-          {listSidebar.map((p) => (
+      {/* we have created another box to use position fixed properly
+      so that there will be no overlap between feed & sidebar */}
+        <Box position="fixed">
+          <List>
+            {listSidebar.map((p) => (
+              <ListItem disablePadding>
+                <ListItemButton component="a" href={p.href}>
+                  <ListItemIcon>{p.icon}</ListItemIcon>
+                  <ListItemText primary={p.primary} />
+                </ListItemButton>
+              </ListItem>
+            ))}
             <ListItem disablePadding>
-              <ListItemButton component="a" href={p.href}>
-                <ListItemIcon>{p.icon}</ListItemIcon>
-                <ListItemText primary={p.primary} />
+              <ListItemButton component="a">
+                <ListItemIcon>
+                  <ModeNight />
+                </ListItemIcon>
+                <Switch />
               </ListItemButton>
             </ListItem>
-          ))}
-          <ListItem disablePadding>
-            <ListItemButton component="a">
-              <ListItemIcon>
-                <ModeNight />
-              </ListItemIcon>
-              <Switch />
-            </ListItemButton>
-          </ListItem>
-        </List>
+          </List>
+        </Box>
       </Box>
     </>
   );
