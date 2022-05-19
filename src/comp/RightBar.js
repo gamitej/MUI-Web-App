bc1e04a03bb24c262b3731781860b63c71b4a857
gamitej <gamitejpratapsingh998@gmail.com>
2022-05-19 15:14:40 +0530

    RightBar.jsx Refactored

--- src/comp/RightBar.js
+++ src/comp/RightBar.js
@@ -9,26 +9,24 @@ import {
 import React from "react";
 import { listRightBarPosts } from "../DataArray/ArrayList";
 import LatestConvo from "./subcomp/LatestConvo";
-import {listRightBarFriends} from '../DataArray/ArrayList'
-
-
+import { listRightBarFriends } from "../DataArray/ArrayList";
 
 const RightBar = () => {
   return (
     <>
       <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
         <Box position="fixed" width={300}>
+          {/* Online Friends */}
           <Typography variant="h6" fontWeight={100}>
             Online Friends
           </Typography>
           <AvatarGroup max={4}>
             {listRightBarFriends.map((item) => (
-              <Avatar
-                alt={item.alt}
-                src={item.src}
-              />
+              <Avatar alt={item.alt} src={item.src} />
             ))}
           </AvatarGroup>
+          {/* Online Friends */}
+          {/* Latest Posts */}
           <Typography variant="h6" fontWeight={100}>
             Latest Posts
           </Typography>
@@ -39,10 +37,13 @@ const RightBar = () => {
               </ImageListItem>
             ))}
           </ImageList>
+          {/* Latest Posts */}
+          {/* Latest Conversations */}
           <Typography variant="h6" fontWeight={100}>
             Latest Conversations
           </Typography>
           <LatestConvo />
+          {/* Latest Conversations */}
         </Box>
       </Box>
     </>
