7d53da08e18f6d544cca3270cdd81c58860fc1cd
gamitej <gamitejpratapsingh998@gmail.com>
2022-05-19 09:54:51 +0530

    Navbar MenuItem & Avatar

--- src/comp/NavBar.jsx
+++ src/comp/NavBar.jsx
@@ -1,16 +1,103 @@
-import { AppBar, styled, Toolbar } from "@mui/material";
-import React from "react";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import { Feed, Mail, Notifications } from "@mui/icons-material";
+import { flexbox } from "@mui/system";
 
 // Customised Toolbar Instead Of Toolbar
 const StyledToolbar = styled(Toolbar)({
-  display:'flex',
-  justifyContent:'space-between'
-})
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+// Customised Search as a Div
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+// Customised Icons as a Box
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+// Customised Icons For xs size
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
 
 const NavBar = () => {
+
+  const [open,setOpen ]= useState(false)
+
   return (
-    <AppBar position="stick">
-      <StyledToolbar>Navbar</StyledToolbar>
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          Social
+        </Typography>
+        <Feed sx={{ display: { xs: "block", sm: "none" } }}></Feed>
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar sx={{ width: 30, height: 30 }} 
+            onClick={e=>setOpen(true)}
+
+          />
+        </Icons>
+        <UserBox onClick={e=>setOpen(true)}>
+          <Avatar sx={{ width: 30, height: 30 }} />
+          <Typography variant="span">Rooney</Typography>
+        </UserBox>
+      </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        // Whenever we are clicking somewhere else
+        onClose={e=>setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
