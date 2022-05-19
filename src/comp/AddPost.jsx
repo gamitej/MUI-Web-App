b4f843dca522668e65ef7f050d65a67764a19a9a
gamitej <gamitejpratapsingh998@gmail.com>
2022-05-19 16:49:24 +0530

    Modal Added setup

--- src/comp/AddPost.jsx
+++ src/comp/AddPost.jsx
@@ -1,12 +1,29 @@
 import styled from "@emotion/styled";
 import { Add } from "@mui/icons-material";
-import { Box, Fab, Modal, Tooltip } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Fab,
+  Modal,
+  TextField,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import React, { useState } from "react";
 
 const StyledModal = styled(Modal)({
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
+});
+
+const UserBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "10px",
+  marginTop: "20px",
+  marginBottom:'20px'
 });
 
 const AddPost = () => {
@@ -38,7 +55,26 @@ const AddPost = () => {
           p={3}
           borderRadius={5}
         >
-          hello
+          <Typography variant="h6" color="gray" textAlign="center">
+            Create Post
+          </Typography>
+          <UserBox>
+            <Avatar
+              src="https://material-ui.com/static/images/avatar/1.jpg"
+              sx={{ width: 30, height: 30 }}
+            />
+            <Typography variant="span" fontWeight={500}>
+              John Doe
+            </Typography>
+          </UserBox>
+          <TextField
+            sx={{ width: "100%" }}
+            id="standard-multiline-static"
+            multiline
+            rows={3}
+            placeholder="What's on your mind ?"
+            variant="standard"
+          />
         </Box>
       </StyledModal>
     </>
