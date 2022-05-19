6d8989376d8d07055372c5a49e2f952336391d0b
gamitej <gamitejpratapsingh998@gmail.com>
2022-05-19 17:04:34 +0530

    App.js Mode Change Dark/Light

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import SideBar from "./comp/SideBar";
 import Feed from "./comp/Feed";
 import RightBar from "./comp/RightBar";
 import NavBar from "./comp/NavBar";
-import { Box, Stack } from "@mui/material";
+import { Box, createTheme, Stack } from "@mui/material";
 import AddPost from "./comp/AddPost";
+import { useState } from "react";
+import { ThemeProvider } from "@emotion/react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+
   return (
-    <Box>
-      <NavBar />
-      <Stack direction="row" spacing={2} justifyContent="space-between">
-        <SideBar />
-        <Feed />
-        <RightBar />
-      </Stack>
-      <AddPost/>
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box>
+        <NavBar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <SideBar />
+          <Feed />
+          <RightBar />
+        </Stack>
+        <AddPost />
+      </Box>
+    </ThemeProvider>
   );
 }
 
