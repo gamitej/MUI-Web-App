cdc124103b970ef5da80173f97c50221fcd3d1a7
gamitej <gamitejpratapsingh998@gmail.com>
2022-05-19 10:51:38 +0530

    Feed - Post.jsx

--- src/comp/Feed.jsx
+++ src/comp/Feed.jsx
@@ -1,58 +1,12 @@
-import { Favorite, MoreVert, Share } from "@mui/icons-material";
-import {
-  Avatar,
-  Box,
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  IconButton,
-  Typography,
-} from "@mui/material";
+import { Box } from "@mui/material";
 import React from "react";
+import Post from './Post'
 
 const Feed = () => {
   return (
     <>
-      <Box  flex={4} p={2}>
-        <Card>
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
-                R
-              </Avatar>
-            }
-            action={
-              <IconButton aria-label="settings">
-                <MoreVert />
-              </IconButton>
-            }
-            title="Shrimp and Chorizo Paella"
-            subheader="September 14, 2016"
-          />
-          <CardMedia
-            component="img"
-            height="10%"
-            image="https://images.pexels.com/photos/9887601/pexels-photo-9887601.jpeg?cs=srgb&dl=pexels-buran%C4%B1nyabanc%C4%B1s%C4%B1-9887601.jpg&fm=jpg"
-            alt="Paella dish"
-          />
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              This impressive paella is a perfect party dish and a fun meal to
-              cook together with your guests. Add 1 cup of frozen peas along
-              with the mussels, if you like.
-            </Typography>
-          </CardContent>
-          <CardActions disableSpacing>
-            <IconButton aria-label="add to favorites">
-              <Favorite />
-            </IconButton>
-            <IconButton aria-label="share">
-              <Share />
-            </IconButton>
-          </CardActions>
-        </Card>
+      <Box flex={4} p={2}>
+        <Post/>
       </Box>
     </>
   );
