520185655b6d3443cacb5e3d0ee45d7da677d343
gamitej <gamitejpratapsingh998@gmail.com>
2022-05-19 15:21:06 +0530

    LatestConvo.jsx Refactored

--- src/comp/subcomp/LatestConvo.jsx
+++ src/comp/subcomp/LatestConvo.jsx
@@ -9,75 +9,60 @@ import {
   Typography,
 } from "@mui/material";
 
+const listLatestConvo = [
+  {
+    alt: "Remy Sharp",
+    src: "/static/images/avatar/1.jpg",
+    primary: "Brunch this weekend?",
+    typo: "Ali Connors",
+    text: " — I'll be in your neighborhood doing errands this…",
+  },
+  {
+    alt: "Travis Howard",
+    src: "/static/images/avatar/2.jpg",
+    primary: "Summer BBQ",
+    typo: "to Scott, Alex, Jennifer",
+    text: " — Wish I could come, but I'm out of town this…",
+  },
+  {
+    alt: "Cindy Baker",
+    src: "/static/images/avatar/3.jpg",
+    primary: "Oui Oui",
+    typo: "Sandra Adams",
+    text: " — Do you have Paris recommendations? Have you ever…",
+  },
+];
+
 const LatestConvo = () => {
   return (
     <>
       <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-        <ListItem alignItems="flex-start">
-          <ListItemAvatar>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-          </ListItemAvatar>
-          <ListItemText
-            primary="Brunch this weekend?"
-            secondary={
-              <>
-                <Typography
-                  sx={{ display: "inline" }}
-                  component="span"
-                  variant="body2"
-                  color="text.primary"
-                >
-                  Ali Connors
-                </Typography>
-                {" — I'll be in your neighborhood doing errands this…"}
-              </>
-            }
-          />
-        </ListItem>
-        <Divider variant="inset" component="li" />
-        <ListItem alignItems="flex-start">
-          <ListItemAvatar>
-            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-          </ListItemAvatar>
-          <ListItemText
-            primary="Summer BBQ"
-            secondary={
-              <>
-                <Typography
-                  sx={{ display: "inline" }}
-                  component="span"
-                  variant="body2"
-                  color="text.primary"
-                >
-                  to Scott, Alex, Jennifer
-                </Typography>
-                {" — Wish I could come, but I'm out of town this…"}
-              </>
-            }
-          />
-        </ListItem>
-        <Divider variant="inset" component="li" />
-        <ListItem alignItems="flex-start">
-          <ListItemAvatar>
-            <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-          </ListItemAvatar>
-          <ListItemText
-            primary="Oui Oui"
-            secondary={
-              <>
-                <Typography
-                  sx={{ display: "inline" }}
-                  component="span"
-                  variant="body2"
-                  color="text.primary"
-                >
-                  Sandra Adams
-                </Typography>
-                {" — Do you have Paris recommendations? Have you ever…"}
-              </>
-            }
-          />
-        </ListItem>
+        {listLatestConvo.map((item) => (
+          <>
+            <ListItem alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+              </ListItemAvatar>
+              <ListItemText
+                primary="Brunch this weekend?"
+                secondary={
+                  <>
+                    <Typography
+                      sx={{ display: "inline" }}
+                      component="span"
+                      variant="body2"
+                      color="text.primary"
+                    >
+                      Ali Connors
+                    </Typography>
+                    {" — I'll be in your neighborhood doing errands this…"}
+                  </>
+                }
+              />
+            </ListItem>
+            <Divider variant="inset" component="li" />
+          </>
+        ))}
       </List>
     </>
   );
