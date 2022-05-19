43b8dd022025260d56e17220aa75933f334923dd
gamitej <gamitejpratapsingh998@gmail.com>
2022-05-19 16:27:54 +0530

    AddPost.jsx Popup Model

--- src/comp/AddPost.jsx
+++ src/comp/AddPost.jsx
@@ -1,15 +1,32 @@
 import { Add } from "@mui/icons-material";
-import { Fab, Tooltip } from "@mui/material";
-import React from "react";
+import { Box, Fab, Modal, Tooltip } from "@mui/material";
+import React, { useState } from "react";
 
 const AddPost = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <Tooltip title="Add New Post" sx={{ position: "fixed" ,bottom:20}} >
+      <Tooltip
+        onClick={(e) => setOpen(true)}
+        title="Add New Post"
+        sx={{
+          position: "fixed",
+          bottom: 20,
+        }}
+      >
         <Fab color="primary" aria-label="add">
-          <Add/>
+          <Add />
         </Fab>
       </Tooltip>
+      <Modal
+        open={open}
+        onClose={(e) => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box>hello</Box>
+      </Modal>
     </>
   );
 };
